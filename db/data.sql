5f7a67d814f72a1e18161e60697bbdeeefddf94f
Tommi Mäkitalo <tommi@tntnet.org>
2015-04-12 21:18:05 +0200

    better example data

--- db/data.sql
+++ db/data.sql
@@ -1,8 +1,54 @@
 insert into book(isbn, title, author, price)
-  values('12345', 'Tntnet - the book', 'Tommi Mäkitalo', 12);
+  values('978-0321992789', 'Programming: Principles and Practice Using C++ (2nd Edition)', 'Bjarne Stroustrup', 50.98);
 insert into book(isbn, title, author, price)
-  values('12346', 'C++ introduction', 'Someone Else', 10);
+  values('978-0321714114', 'C++ Primer (5th Edition)', 'Stanley B. Lippman', 43.97);
 insert into book(isbn, title, author, price)
-  values('1995', 'The gardenbook', 'Lina Mäkitalo', 11);
+  values('978-0321958310', 'A Tour of C++ (C++ In-Depth Series)', 'Bjarne Stroustrup', 23.69);
 insert into book(isbn, title, author, price)
-  values('1505', 'Advanced english', 'Kirsti Mäkitalo', 11);
+  values('860-1400402207', 'Accelerated C++: Practical Programming by Example', 'Andrew Koenig', 32.46);
+insert into book(isbn, title, author, price)
+  values('860-1300092737', 'Thinking in C++: Introduction to Standard C++, Volume One (2nd Edition) (Vol 1)', 'Bruce Eckel', 56.00);
+insert into book(isbn, title, author, price)
+  values('978-0321334879', 'Effective C++: 55 Specific Ways to Improve Your Programs and Designs (3rd Edition)', 'Scott Meyers', 35.22);
+insert into book(isbn, title, author, price)
+  values('078-5342749625', 'Effective STL: 50 Specific Ways to Improve Your Use of the Standard Template Library', 'Scott Meyers', 44.92);
+insert into book(isbn, title, author, price)
+  values('078-5342633719', 'More Effective C++: 35 New Ways to Improve Your Programs and Designs', 'Scott Meyers', 42.69);
+insert into book(isbn, title, author, price)
+  values('978-0201615623', 'Exceptional C++: 47 Engineering Puzzles, Programming Problems, and Solutions', 'Herb Sutter', 44.92);
+insert into book(isbn, title, author, price)
+  values('978-0201704341', 'More Exceptional C++: 40 New Engineering Puzzles, Programming Problems, and Solutions', 'Herb Sutter', 38.11);
+insert into book(isbn, title, author, price)
+  values('078-5342760422', 'Exceptional C++ Style: 40 New Engineering Puzzles, Programming Problems, and Solutions', 'Herb Sutter', 45.63);
+insert into book(isbn, title, author, price)
+  values('007-6092018117', 'C++ Coding Standards: 101 Rules, Guidelines, and Best Practices', 'Herb Sutter and Andrei Alexandrescu', 44.81);
+insert into book(isbn, title, author, price)
+  values('978-0201734843', 'C++ Templates: The Complete Guide', 'David Vandevoorde and Nicolai M. Josuttis', 58.11);
+insert into book(isbn, title, author, price)
+  values('978-1491903995', 'Effective Modern C++: 42 Specific Ways to Improve Your Use of C++11 and C++14', 'Scott Meyers', 41.14);
+insert into book(isbn, title, author, price)
+  values('978-0201704310', 'Modern C++ Design: Generic Programming and Design Patterns Applied', 'Andrei Alexandrescu', 51.27);
+insert into book(isbn, title, author, price)
+  values('978-0321227256', 'C++ Template Metaprogramming: Concepts, Tools, and Techniques from Boost and Beyond', 'David Abrahams and Aleksey Gurtovoy', 44.81);
+insert into book(isbn, title, author, price)
+  values('860-1200915495', 'C++ Concurrency in Action: Practical Multithreading', 'Anthony Williams', 58.98);
+insert into book(isbn, title, author, price)
+  values('978-1460966167', 'Advanced C++ Metaprogramming', 'Davide Di Gennaro', 43.98);
+insert into book(isbn, title, author, price)
+  values('978-0201700732', 'The C++ Programming Language: Special Edition (3rd Edition)', 'Bjarne Stroustrup', 62.99);
+insert into book(isbn, title, author, price)
+  values('978-0321623218', 'The C++ Standard Library: A Tutorial and Reference (2nd Edition)', 'Nicolai M. Josuttis', 51.54);
+insert into book(isbn, title, author, price)
+  values('978-0201183955', 'Standard C++ IOStreams and Locales: Advanced Programmer''s Guide and Reference', 'Angelika Langer and Klaus Kreft', 23.94);
+insert into book(isbn, title, author, price)
+  values('978-0201543308', 'The Design and Evolution of C++', 'Bjarne Stroustrup', 52.68);
+insert into book(isbn, title, author, price)
+  values('978-0201423396', 'Ruminations on C++: A Decade of Programming Insight and Experience', 'Andrew Koenig', 32.05);
+insert into book(isbn, title, author, price)
+  values('978-0201548556', 'Advanced C++ Programming Styles and Idioms', 'James O. Coplien', 45.53);
+insert into book(isbn, title, author, price)
+  values('078-5342633627', 'Large-Scale C++ Software Design', 'John Lakos', 53.88);
+insert into book(isbn, title, author, price)
+  values('078-5342834543', 'Inside the C++ Object Model', 'Stanley B. Lippman', 43.85);
+insert into book(isbn, title, author, price)
+  values('978-0321133540', 'Beyond the C++ Standard Library: An Introduction to Boost', 'Björn Karlsson', 59.49);
